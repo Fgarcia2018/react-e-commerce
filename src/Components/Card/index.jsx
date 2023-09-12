0b6f4c4d78498ae12a08c2280f0b522cdfb4c74d
Fernando García <andgarcia_24@hotmail.com>
2023-09-12 12:34:45 -0500

    Se consume API FAKE de PLATZI para que el componente card sea dinámico.

--- src/Components/Card/index.jsx
+++ src/Components/Card/index.jsx
@@ -1,14 +1,14 @@
-const Card=()=>{
+const Card=(data)=>{
     return(
         <div className="bg-white cursor-pointer w-56 h-60">
             <figure className="relative mb-2 w-full h-4/5">
-                <span className="absolute bottom-0 left-0 bg-white/60 rounded-lg text-black text-xs m-2 px-3 py-0.5">Electronics </span>
-                <img className="w-full h-full object-cover rounded-lg" src="https://media.wired.com/photos/6014a6587201ff6c1ba30bcb/master/w_320,c_limit/Gear-Apple-Airpods-Max-Angle-SOURCE-Apple.jpg" alt="headphones" />
+                <span className="absolute bottom-0 left-0 bg-white/60 rounded-lg text-black text-xs m-2 px-3 py-0.5">{data.data.category.name} </span>
+                <img className="w-full h-full object-cover rounded-lg" src={data.data.images[0]} alt={data.data.title} />
                 <div className="absolute top-0 right-0 flex justify-center items-center bg-white w-6 h-6 rounded-full m-2 p-1">+</div>                
             </figure>
             <p className="flex justify-between">
-                <span className="text-sm font-light">Headphones</span>
-                <span className="text-lg font-medium">$300</span>
+                <span className="text-sm font-light">{data.data.title}</span>
+                <span className="text-lg font-medium">${data.data.price}</span>
             </p>
         </div>
 
