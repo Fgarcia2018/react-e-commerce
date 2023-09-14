6eab6ce008ebdd333c045d484430da0cd76a87e3
Fernando García <andgarcia_24@hotmail.com>
2023-09-14 12:15:17 -0500

    Se crea estado para mostrar información en ProductDetail dependiendo del Card seleccionado.

--- src/Components/ProductDetail/index.jsx
+++ src/Components/ProductDetail/index.jsx
@@ -3,6 +3,7 @@ import './styles.css';
 import { ShoppingCartContext } from '../../Context';
 const ProductDeatail=()=>{
     const context=useContext(ShoppingCartContext)
+
     return(
         <aside className={`${context.isProductDetailOpen?'flex':'hidden'} product-detail flex-col fixed right-0 border border-black rounded-lg bg-white p-6`}>
             <div className='flex justify-between items-center'>
@@ -10,6 +11,16 @@ const ProductDeatail=()=>{
                 <div className="absolute top-0 right-0 flex justify-center items-center bg-white w-6 h-6 rounded-full m-2 p-1 cursor-pointer" onClick={()=>context.closeProductDetail()}>
                     <img src='../../assets/closeIcon.svg'/></div>
             </div>
+            <figure className='px-6'>
+                <img className="w-full h-full rounded-lg" 
+                src={context.productToShow.images[0]}
+                 alt={context.productToShow.title} />                 
+            </figure>
+            <p className='flex flex-col p-6'> 
+                <span className='font-medium text-2xl'>${context.productToShow.price}</span>
+                <span className='font-medium text-xl'>{context.productToShow.title}</span>
+                <span className='font-light text-sm'>{context.productToShow.description}</span>
+            </p>
         </aside>
 
     )
--- src/Context/index.jsx
+++ src/Context/index.jsx
@@ -3,10 +3,21 @@ import { createContext, useState } from "react";
 export const ShoppingCartContext=createContext();
 
 export const ShoppingCartProvider=({children})=>{
+    // Shopping Cart - Increment Quantity
     const [counter,setCounter]=useState(0);
+
+    // Product Detail - Open/Close
     const [isProductDetailOpen,setIsProductDetailOpen]=useState(false);
     const openProductDetail=()=>setIsProductDetailOpen(true);
     const closeProductDetail=()=>setIsProductDetailOpen(false);
+
+    // Product Detal - Show Product
+    const [productToShow,setProductToShow]=useState({
+        images:[],
+        title:'',
+        description:'',
+        price:''});
+
 
     return(
         <ShoppingCartContext.Provider value={
@@ -15,7 +26,9 @@ export const ShoppingCartProvider=({children})=>{
                 setCounter,
                 isProductDetailOpen,
                 openProductDetail,
-                closeProductDetail
+                closeProductDetail,
+                productToShow,
+                setProductToShow
             }
         }>
             {children}
